07cbe0c06619b425343c03f3a55f8a17e5e98934
Felice Forby <feliceforby@gmail.com>
2018-05-08 22:55:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   end
   
   def self.find_by_name(searched_name)
-    @@all.find{ |song| song.name == searched_name }
+    self.all.find{ |song| song.name == searched_name }
   end
   
   def self.find_or_create_by_name(song_name)
