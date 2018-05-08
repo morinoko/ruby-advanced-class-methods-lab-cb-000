be7d672633b07b12d1772602fd25ddac7af5c978
Felice Forby <feliceforby@gmail.com>
2018-05-08 22:42:45 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,60 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    song.save
+    song
+  end
+  
+  def self.find_by_name(searched_name)
+    @@all.find{ |song| song.name == searched_name }
+  end
+  
+  def self.find_or_create_by_name(song_name)
+    if self.find_by_name(song_name) == nil
+      self.create_by_name(song_name)
+    else
+      self.find_by_name(song_name)
+    end
+  end
+  
+  def self.alphabetical
+    @@all.sort_by{ |song| song.name } 
+  end
+  
+  # Create song from "Taylor Swift - Blank Space.mp3" file format
+  def self.new_from_filename(filename)
+    parsed_filename = filename.chomp(".mp3").split(" - ")
+    artist_name = parsed_filename[0]
+    name = parsed_filename[1]
+    
+    song = self.new
+    song.name = name
+    song.artist_name = artist_name
+    song
+  end
+  
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    song.save
+  end
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
 end
